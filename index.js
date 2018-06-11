93fbce990d3564e96f9516491073c86173aea17a
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2018-06-11 22:02:58 +0200

    Use port from env

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
 
 
-
-var server = app.listen(3000, function () {
-  console.log('Example app listening at http://localhost:3000');
+var server = app.listen(process.env.PORT || 3000, function(){
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 app.get('/', function (req, res) {
