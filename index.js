ab794e882c8eac42122e7888d68f50a7c7bc03ac
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2018-06-12 20:39:22 +0200

    Add bulma css framework

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var path = require('path');
 var hbs = require('hbs');
+var fs = require('fs');
+
+// Register Partials
+var partialsDir = __dirname + '/views/partials';
+var filenames = fs.readdirSync(partialsDir);
+
+filenames.forEach(function (filename) {
+  var matches = /^([^.]+).hbs$/.exec(filename);
+  if (!matches) {
+    return;
+  }
+  var name = matches[1];
+  var template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
+  hbs.registerPartial(name, template);
+});
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -8,13 +23,11 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
 
 
+
 var server = app.listen(process.env.PORT || 3000, function(){
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 app.get('/', function (req, res) {
-  res.render('index', {
-    headline: 'Ancestry Tree',
-    images: []
-  });
+  res.render('index');
 });
